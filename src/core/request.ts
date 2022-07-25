0f1b192109e57d6958c93b981d9138db4cb7a090
Oskar Lindgren <2677573+MrOggy85@users.noreply.github.com>
2022-07-26 06:09:39 +0900

    chore: add BASE_URL & AUTH env

--- src/core/request.ts
+++ src/core/request.ts
@@ -1,5 +1,19 @@
 import axios, { Method } from 'axios';
 
+const REACT_APP_BASE_URL = process.env.REACT_APP_BASE_URL
+console.log('REACT_APP_BASE_URL', REACT_APP_BASE_URL);
+
+if (!REACT_APP_BASE_URL) {
+  throw new Error('REACT_APP_BASE_URL is empty!')
+}
+
+const REACT_APP_AUTH = process.env.REACT_APP_AUTH
+  console.log('REACT_APP_AUTH', REACT_APP_AUTH);
+
+  if (!REACT_APP_AUTH) {
+    throw new Error('REACT_APP_AUTH is empty!')
+  }
+
 type Params = {
   path: string;
   method: Method;
@@ -7,9 +21,22 @@ type Params = {
 };
 
 async function request({ path, method, data }: Params) {
+  const baseURL = REACT_APP_BASE_URL
+  if (!baseURL) {
+    throw new Error('No baseURL!');
+  }
+
+  const Authorization = REACT_APP_AUTH
+  if (!Authorization) {
+    throw new Error('No Authorization Header!');
+  }
+
   const response = await axios({
     url: path,
-    baseURL: 'http://localhost:8000',
+    baseURL,
+    headers: {
+      Authorization,
+    },
     method,
     data,
   });
